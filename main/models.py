c243cc438c7eb8a4c5ef67e3ef3a52a8875ce4d4
tonyonce <tonyonce@qq.com>
2018-12-05 19:29:10 +0800

    修改models, 然后后台管理可以显示主题和条目时显示的是中文, 增加可读性

--- main/models.py
+++ main/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """用户学习的主题"""
-    text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200, verbose_name='主题名称')
+    date_added = models.DateTimeField(auto_now=True, editable=False, verbose_name='创建时间')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='创建人')
 
     def __str__(self):
         """返回模型的字符串表示"""
@@ -16,18 +16,19 @@ class Topic(models.Model):
     class Meta:
         """定义数据表名称"""
         db_table = "topic"
+        verbose_name = verbose_name_plural = '主题'
 
 
 class Entry(models.Model):
     """某个主题的特定知识"""
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50,default='无标题')
-    text = models.TextField()
-    date_added = models.DateTimeField(auto_now=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, verbose_name='选择主题')
+    title = models.CharField(max_length=50,default='无标题', verbose_name='标题')
+    text = models.TextField(verbose_name='内容')
+    date_added = models.DateTimeField(auto_now=True, verbose_name='创建时间')
 
     class Meta:
         db_table = 'entry'
-        verbose_name_plural = 'entries'
+        verbose_name = verbose_name_plural = '条目'
 
     def __str__(self):
         """返回默认显示"""
